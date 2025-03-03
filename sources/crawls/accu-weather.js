453d1e1827a6d57eaf94e41d9d093ffec37815d8
quan <nnmhqn2003@gmail.com>
2025-03-03 23:17:17 +0700

    add Dockerfile

--- sources/crawls/accu-weather.js
+++ sources/crawls/accu-weather.js
@@ -13,7 +13,10 @@ export const getWeather = async () => {
         "--single-process",
         "--no-zygote",
       ],
-      executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath()
+      executablePath: 
+        process.env.NODE_ENV === "production" 
+            ? (process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/chromium-browser") // Mặc định là Chromium
+            : puppeteer.executablePath()
     });
     const page = await browser.newPage(); 
 
